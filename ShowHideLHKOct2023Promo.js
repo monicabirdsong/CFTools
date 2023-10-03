0a5d90f58f7329f7bc911f9b8dd259cf6921f2b4
monicabirdsong <monicabirdsong@gmail.com>
2023-10-03 09:17:36 -0500

    Update ShowHideLHKOct2023Promo.js

--- ShowHideLHKOct2023Promo.js
+++ ShowHideLHKOct2023Promo.js
@@ -4,8 +4,11 @@
   const currentTime = new Date(now);
 
   // Define the show and hide moments in Eastern Time
-  const showTime = new Date('October 19, 2023 18:00:00 EST');
-  const hideTime = new Date('October 24, 2023 12:01:00 EST');
+  //const showTime = new Date('October 19, 2023 18:00:00 EST');
+  //const hideTime = new Date('October 24, 2023 12:01:00 EST');
+
+  const showTime = new Date('October 19, 2023 10:20:00 EST');
+  const hideTime = new Date('October 24, 2023 10:21:00 EST');
 
   // Find the DIV with data-title="Oct2023Promo"
   const octPromoDiv = document.querySelector('div[data-title="Oct2023Promo"]');
